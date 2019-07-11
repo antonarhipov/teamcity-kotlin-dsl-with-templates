670c9658ad26fc8c5de997a713c69d4e79898e55
Anton Arhipov <ant.arhipov@gmail.com>
2019-07-11 13:05:26 +0300

    Update settings.kts

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -2,34 +2,10 @@ import jetbrains.buildServer.configs.kotlin.v2018_2.*
 import jetbrains.buildServer.configs.kotlin.v2018_2.buildSteps.placeholder
 import jetbrains.buildServer.configs.kotlin.v2018_2.buildSteps.script
 
-/*
-The settings script is an entry point for defining a TeamCity
-project hierarchy. The script should contain a single call to the
-project() function with a Project instance or an init function as
-an argument.
-
-VcsRoots, BuildTypes, Templates, and subprojects can be
-registered inside the project using the vcsRoot(), buildType(),
-template(), and subProject() methods respectively.
-
-To debug settings scripts in command-line, run the
-
-    mvnDebug org.jetbrains.teamcity:teamcity-configs-maven-plugin:generate
-
-command and attach your debugger to the port 8000.
-
-To debug in IntelliJ Idea, open the 'Maven Projects' tool window (View
--> Tool Windows -> Maven Projects), find the generate task node
-(Plugins -> teamcity-configs -> teamcity-configs:generate), the
-'Debug' option is available in the context menu for the task.
-*/
-
 version = "2019.1"
 
 project {
-
     buildType(Build)
-
     template(MyTemplate)
 }
 
@@ -43,11 +19,7 @@ object Build : BuildType({
 
     steps {
         script {
-            id = "RUNNER_7"
             scriptContent = """echo "running the Build...""""
-            param("org.jfrog.artifactory.selectedDeployableServer.downloadSpecSource", "Job configuration")
-            param("org.jfrog.artifactory.selectedDeployableServer.useSpecs", "false")
-            param("org.jfrog.artifactory.selectedDeployableServer.uploadSpecSource", "Job configuration")
         }
     }
 })
@@ -57,21 +29,12 @@ object MyTemplate : Template({
 
     steps {
         script {
-            id = "RUNNER_4"
             scriptContent = """echo "Hello from MyTemplate""""
-            param("org.jfrog.artifactory.selectedDeployableServer.downloadSpecSource", "Job configuration")
-            param("org.jfrog.artifactory.selectedDeployableServer.useSpecs", "false")
-            param("org.jfrog.artifactory.selectedDeployableServer.uploadSpecSource", "Job configuration")
         }
         placeholder {
-            id = "RUNNER_6"
         }
         script {
-            id = "RUNNER_5"
             scriptContent = """echo "Final step from MyTemplate""""
-            param("org.jfrog.artifactory.selectedDeployableServer.downloadSpecSource", "Job configuration")
-            param("org.jfrog.artifactory.selectedDeployableServer.useSpecs", "false")
-            param("org.jfrog.artifactory.selectedDeployableServer.uploadSpecSource", "Job configuration")
         }
     }
 })
