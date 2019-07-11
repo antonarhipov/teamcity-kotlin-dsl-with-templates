3ed81febf5c6a03cd0f5976b8ea5930428c65d79
Anton Arhipov <ant.arhipov@gmail.com>
2019-07-11 13:28:37 +0300

    template deleted

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -6,11 +6,9 @@ version = "2019.1"
 
 project {
     buildType(Build)
-    template(MyTemplate)
 }
 
 object Build : BuildType({
-    templates(MyTemplate)
     name = "Build"
 
     vcs {
@@ -20,21 +18,6 @@ object Build : BuildType({
     steps {
         script {
             scriptContent = """echo "running the Build...""""
-        }
-    }
-})
-
-object MyTemplate : Template({
-    name = "MyTemplate"
-
-    steps {
-        script {
-            scriptContent = """echo "Hello from MyTemplate""""
-        }
-        placeholder {
-        }
-        script {
-            scriptContent = """echo "Final step from MyTemplate""""
         }
     }
 })
